5f858335e59e44ecb812deabfcb3416ec147d8b9
Alice Wyan <121136+wyan@users.noreply.github.com>
2025-01-10 10:29:55 +0100

    Ejemplo con fetch y promesas

--- fetch.js
+++ fetch.js
@@ -1,18 +1,17 @@
 "use strict"
 
-let respuesta = {}
+document.addEventListener('DOMContentLoaded', () => {
+    let respuesta = {}
 
-console.log(`Antes de fetch: ${JSON.stringify(respuesta)}`)
+    console.log(`Antes de fetch: ${JSON.stringify(respuesta)}`)
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => {
+            respuesta = json;
+            console.log(`Al acabar la request: ${JSON.stringify(respuesta)}`)
+        })
+        .catch(error => console.log('Ocurrió un error ¯\\_(ツ)_/¯'));
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-.then(response => response.json())
-.then(json => {
-    respuesta = json;
-    console.log(`Al acabar la request: ${JSON.stringify(respuesta)}`)
+    setTimeout(() => console.log(`Al acabar el contador: ${JSON.stringify(respuesta)}`), 5);
+    console.log(`Despues de fetch: ${JSON.stringify(respuesta)}`)
 });
-
-setTimeout(() => {
-    console.log(`Al acabar el contador: ${JSON.stringify(respuesta)}`)
-}, 5);
-
-console.log(`Despues de fetch: ${JSON.stringify(respuesta)}`)
