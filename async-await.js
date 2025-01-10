de493c6acb50b6fd9f6b9758819f30414e133f8e
ALICE WYAN GARCIA MARTIN <alicew.garmar@educa.jcyl.es>
2025-01-10 14:05:23 +0100

    Async/await al rescate

--- async-await.js
+++ async-await.js
@@ -1,14 +1,39 @@
 "use strict"
 
+function pintarPost(respuesta) {
+    let h1 = document.createElement('h1');
+    h1.textContent = respuesta.title;
+
+    let p = document.createElement('p');
+    p.textContent = respuesta.body;
+
+    let div = document.querySelector('.main');
+    div.appendChild(h1);
+    div.appendChild(p);
+}
+
+function pintarComentario(comentario) {
+    let h2 = document.createElement('h2')
+    h2.textContent = comentario.name
+
+    let email = document.createElement('p')
+    email.textContent = comentario.email
+
+    let body = document.createElement('p')
+    body.textContent = comentario.body
+
+    let div = document.querySelector('.main');
+    div.appendChild(h2)
+    div.appendChild(email)
+    div.appendChild(body)
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
-    console.log(`Antes de fetch: ${JSON.stringify(respuesta)}`)
-
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    console.log(`Estado de la respuesta: ${response.status}`);
-
-    const json = await response.json();
-    console.log(`Al acabar la request: ${JSON.stringify(respuesta)}`)
-
-    setTimeout(() => console.log(`Al acabar el contador: ${JSON.stringify(respuesta)}`), 5);
-    console.log(`Tras establecer el contador: ${JSON.stringify(respuesta)}`)
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+    const respuesta = await res.json();
+    pintarPost(respuesta);
+   
+    const res2 = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments');
+    const comentarios = await res2.json();
+    comentarios.forEach(pintarComentario);
 });
